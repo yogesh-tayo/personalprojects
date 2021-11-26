16036a60856c1dff082eec7698e3dcd48e8cadec
yogesh <yogesh.kumar1@magicbricks.com>
2021-11-26 19:57:44 +0530

    dollar project

---
+++ dollarproject/project1.js
@@ -0,0 +1,21 @@
+
+let mass=document.getElementById("BMI");
+let height=document.getElementById("Res1");
+let button=document.getElementById("btn");
+// function bmi(){
+mass.addEventListener('input',function(){
+    let h=this.value;
+    let re=h*70;
+    height.value=re.toFixed(2);
+    
+      
+    
+
+});
+height.addEventListener('input',function(){
+    let m=this.value;
+    let re1=m/70;
+    mass.value=re1.toFixed(2);
+    
+
+});
